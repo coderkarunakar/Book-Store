da536ecdb06f44340a4e1ec0bf9d9d6746b043f7
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-11-14 07:55:07 +0530

    Reapply "created first http route and connected to DB"
    This reverts commit 3ca5f164af9b0ecc9bb64333745501471e133a0c.

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export  const PORT = 5555;
+export const MONGODBURL = 'mongodb+srv://chembetikarunakar:2Ox3AE3ktte62Umv@mernapp.qhykh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=mernapp'
