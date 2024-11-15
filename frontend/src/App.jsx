a5cf6aeb2ea9167e411fcb2d1c4f1e95da0ea8c7
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-11-15 10:44:59 +0530

    created react project , vite ,Tailwind Css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+//note:the short to create below 4 line are type rafc and click enter
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div className='bg-red-400 text-white'>App</div>
+  )
+}
+
+export default App
