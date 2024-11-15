ab1b5cac58796cc13df67ef5bfb930ea38004400
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-11-15 11:22:12 +0530

    SPA and add react  router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 //note:the short to create below 4 line are type rafc and click enter
 import React from 'react'
-
+import {Routes,Route} from 'react-router-dom'
+import { Home } from './pages/Home'
+import { CreateBook } from './pages/CreateBook'
+import { DeleteBook } from './pages/DeleteBook'
+import { ShowBook } from './pages/ShowBook'
+import { EditBook } from './pages/EditBook'
 export const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
-  )
+    <Routes>
+      <Route path ='' element={<Home/>} />
+      <Route path ='' element={<CreateBook/>} />
+      <Route path ='' element={<ShowBook/>} />
+      <Route path ='' element={<EditBook/>} />
+      <Route path ='' element={<DeleteBook/>} />
+    </Routes>
+    )
 }
 
 export default App
